7583b208b2f60b402069d57639815e786b34f9fc
Dan Song <blueintelli@gmail.com>
2019-12-02 11:25:49 +0900

    Change digit to Hangul

--- main.js
+++ main.js
@@ -1,3 +1,35 @@
 function changeHangul() {
   alert("changeHangul!!");
 }
+
+function chnageDigitToHangul(number) {
+  if (number === 0) {
+    return "영";
+  } else if (number === 1) {
+    return "일";
+  } else if (number === 2) {
+    return "이";
+  } else if (number === 3) {
+    return "삼";
+  } else if (number === 4) {
+    return "사";
+  } else if (number === 5) {
+    return "오";
+  } else if (number === 6) {
+    return "육";
+  } else if (number === 7) {
+    return "칠";
+  } else if (number === 8) {
+    return "팔";
+  } else if (number === 9) {
+    return "구";
+  }
+}
+
+function changeNumberToHangul(number) {
+  let result = "";
+  if (number < 10) {
+    result = chnageDigitToHangul(number);
+  }
+  return result;
+}
--- tests.js
+++ tests.js
@@ -1,3 +1,12 @@
-QUnit.test("hello test", function(assert) {
-  assert.ok(1 == "1", "Passed!");
+QUnit.test("hangul change test", function(assert) {
+  assert.equal(changeNumberToHangul(1), "일");
+  assert.equal(changeNumberToHangul(2), "이");
+  assert.equal(changeNumberToHangul(3), "삼");
+  assert.equal(changeNumberToHangul(4), "사");
+  assert.equal(changeNumberToHangul(5), "오");
+  assert.equal(changeNumberToHangul(6), "육");
+  assert.equal(changeNumberToHangul(7), "칠");
+  assert.equal(changeNumberToHangul(8), "팔");
+  assert.equal(changeNumberToHangul(9), "구");
+  assert.equal(changeNumberToHangul(10), "십");
 });
